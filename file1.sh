7cf9e331ade2e871733f0cd49a626ee83518e423
Hadi Komeili <komeili.hadi@gmail.com>
2021-04-30 21:16:31 +0430

    fix file1.sh

--- file1.sh
+++ file1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "hi world"
-name = 'HADI'
+name='HADI'
 echo -n 'my name is '$name
-echo -e 'my age is: \n 25 \n my weight \t is : \t 70'
-: ' comment'
+echo -e '\nmy age is: 25 \nmy weight is : 70'
+: ' commentggggggg'
 # sdjgdvsbf
 echo "height:"
 read height
 echo "my height is: "$height
-echo "goodbye"$1
+echo "goodbye "$name
